c07f5499f4ed655a676a6caa11b6b223fba31d91
саня высочина <2001sashka@tut.by>
2019-03-12 20:56:00 +0300

    first commit

---
+++ switch-statement/switch-statement/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace switch_statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Color color =(Color) (new System.Random().Next(0, 3));
+            Console.WriteLine(color);
+            Console.Read();
+        }
+    }
+
+    public enum Color { Red, Green, Blue}
+    public class Random { } 
+}
