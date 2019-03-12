3388da3d61ae83b2cd4cb28687736258931dc626
саня высочина <2001sashka@tut.by>
2019-03-12 21:51:18 +0300

    first commit

---
+++ for/for/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter word");
+            string word = Console.ReadLine();
+            int count = 0;
+            int a = word.Length;
+            for (int i = 0; i < a; i++)
+            {
+                if (word[i] == 'a')
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+            Console.Read();
+
+        }
+    }
+}
