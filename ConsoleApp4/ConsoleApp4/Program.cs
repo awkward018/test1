f9eac4f03ff34b96d60aeafb5171fcf170514e95
саня высочина <2001sashka@tut.by>
2019-03-21 21:47:03 +0300

    commit

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int temperature = -3;
+            string city = "Minsk";
+            DayOfWeek dt = DateTime.Now.DayOfWeek;
+            string customMessage =$"It's {temperature} degrees, in {city} on this {dt}";
+            Console.WriteLine(customMessage);
+            Console.ReadLine();
+        }
+    }
+}
